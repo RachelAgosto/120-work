9d62217c25b29d0f60d78f8a8c84072d244043dd
Rachel Agosto <31681417+RachelAgosto@users.noreply.github.com>
2017-09-24 15:57:32 -0600

    Add files via upload

---
+++ hw2/hw-3/week-3/01_hello-world/sketch.js
@@ -0,0 +1,16 @@
+// This is a setup functiion
+function setup() {
+
+    // This here canvas is 600px wide and 400px high
+    createCanvas( 600, 400 );
+
+    // I shall make it's background 'purple'
+    background( 'purple' );
+}
+// I am now going to declare the draw function
+function draw() {
+
+    // Imma tell p5 to print this here text string "Hello World!"
+    // This is going to be positioned at ( x: 300, y: 200 ).
+    text( "Hello World!", 300, 200 );
+}
